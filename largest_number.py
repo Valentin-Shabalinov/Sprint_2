e5b64f9eaa67bf7110d3cee59e66f73ebc8ed2b8
Valentin-Shabalinov <111199259+Valentin-Shabalinov@users.noreply.github.com>
2022-12-24 18:58:55 +0300

    Add files via upload

--- largest_number.py
+++ largest_number.py
@@ -4,10 +4,9 @@ from typing import Optional
 
 def largest_number(amount: Optional[int], numbers: str) -> str:
     result = []
-    for number in numbers:
-        for permutation in itertools.permutations(str(number)):
-            result.append(''.join(permutation))
-        return max(result)
+    for permutation in itertools.permutations(str(number) for number in numbers):
+        result.append(''.join(permutation))
+    return max(result)
 
 
 print(largest_number(input(), input()))
